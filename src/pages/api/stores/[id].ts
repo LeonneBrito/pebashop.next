d5620157febeca7a6406437d6c2b7169a1cbdf9a
Leonne Brito <contato@leonnebrito.com.br>
2021-05-06 19:00:25 -0300

    :tada: feat: finished shop crud

--- src/pages/api/stores/[id].ts
+++ src/pages/api/stores/[id].ts
@@ -7,6 +7,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const {
     query: { id },
     method,
+    body,
   } = req
 
   switch (method) {
@@ -22,6 +23,40 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           return res.status(400).json({ message: 'A loja não existe.' })
         }
         return res.json(store);
+      } catch(error) {
+        return res.status(500).json({error: error.message});
+      }
+      break
+    case 'PUT':
+      try {
+        const store = await prisma.store.update({
+          where: {
+            id: String(id)
+          },
+          data: {
+            name: body.name,
+            latitude: body.latitude,
+            longitude: body.longitude,
+            about: body.about,
+            opening_hours: body.opening_hours,
+            open_on_weekends: body.open_on_weekends,
+            whatsapp: body.whatsapp,
+            image: body.image,
+          }
+        })
+        return res.json(store);
+      } catch(error) {
+        return res.status(500).json({error: error.message});
+      }
+      break
+    case 'DELETE':
+      try {
+        const store = await prisma.store.delete({
+          where: {
+            id: String(id)
+          }
+        })
+        res.status(204);
       } catch(error) {
         return res.status(500).json({error: error.message});
       }
